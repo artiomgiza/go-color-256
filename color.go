919180511aab949420f920485b79c690f4bf0034
Art Giz <artiomg@gett.com>
2019-02-27 22:22:49 +0200

    upd pic

--- color.go
+++ color.go
@@ -2,6 +2,8 @@ package color
 
 import "fmt"
 
+// use colors.png picture to chose the color number
+
 func AddColor(s string, color int) string {
 	return fmt.Sprintf("\x1b[38;5;%dm%s\x1b[0m", color, s)
 }
